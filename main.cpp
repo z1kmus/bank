0bec0888fc187bc7adc7d0afade4b4e92ba8a165
z1kmus <workweb.2005@gmail.com>
2025-05-05 17:14:38 +0500

    Update main.cpp
    Yakuniy mahsulot

--- main.cpp
+++ main.cpp
@@ -50,6 +50,10 @@ class Client{
 		this->bolim_id = id;
 	}
 
+	string getName(){
+		return name;
+	}
+
 	
 };
 
@@ -65,52 +69,101 @@ class Bank{
 
 	void addClient(string name, int id){
 		navbat.at(id-1).push(Client(name, id));
+		cout<<"Navbatga muvafaqqiyatli qo'shildingiz!"<<endl;
 	}
 
 	void printBolim(){
 		bank.print();
+		cout<<"----------------------------------------------"<<endl;
 	}
 
 	void addBolim(string bolim){
 		bank.add(bolim);
+		cout<<"Bo'lim muvafaqqiyatli qo'shildi!"<<endl;
+		
 	}
 
 	
 	void getSize(){
 		cout<<navbat.size()<<endl;
 	}
+
+	void servise(){
+		for(int i=0;i<navbat.size(); i++){
+			if(!navbat.at(i).empty()){
+				cout<<bank.getAsMap().at(i+1)<<" => "<<navbat.at(i).front().getName()<<" ga hizmat ko'rsatildi. "<<endl;
+				navbat.at(i).pop();
+			}
+		}
+		cout<<"----------------------------------------------"<<endl;
+	}
+
 	void getInfo();
+	void getInfo(int bolim);
 };
 
 
 
 int main(){
-	// Bolim bank;
-	// bank.add("Kassa");
-	// bank.print();
-	// cout<<endl;
 	Bank tbc;
-	tbc.addBolim("Kassa");
-	tbc.printBolim();
-	tbc.addClient("Mustafo", 1);
-	tbc.addClient("Mustafo", 3);
-	tbc.getInfo();
+	map<int, string> data;
+	data[1] = "Bank bolimlari ro'yhati";
+	data[2] = "Bolim qo'shish";
+	data[3] = "Navbatga yozilish";
+	data[4] = "Navbat bo'yicha malumot olish";
+	data[5] = "Xizmat ko'rsatish";
+	data[6] = "Exit";
+	cout<<"--------> TBC bankiga xush kelibsiz! <--------"<<endl;
+	cout<<"Mavjud hizmatlar:"<<endl;
+	for(auto i : data){
+		cout<<setw(3)<<i.first<<". "<<i.second<<endl;
+	}
+	cout<<"----------------------------------------------"<<endl;
+	while (true){
+		int n;
+		cout<<"Hizmatni tanlang: "; cin>>n;
+		cout<<"----------------------------------------------"<<endl;
+		if(n == 1){
+			tbc.printBolim();
+		}
+		else if(n == 2){
+			cin.ignore();
+			string name;
+			cout<<"Bolim nomi: "; getline(cin, name);
+			tbc.addBolim(name);
+			cout<<"----------------------------------------------"<<endl;
+		}
+		else if(n == 3){
+			cin.ignore();
+			string name;
+			int b;
+			cout<<"Name: "; getline(cin, name);
+			cout<<"Bolim (int): "; cin>>b;
+			tbc.addClient(name, b);
+			cout<<"----------------------------------------------"<<endl;
+		}
+		else if(n == 4){
+			cin.ignore();
+			int b;
+			cout<<"Bolim raqami: "; cin>>b;
+			tbc.getInfo();
+			tbc.getInfo(b);
+		}
+		else if(n == 5){
+			tbc.servise();
+		}
+		else if(n == 6){
+			cout<<"Bankimiz hizmatlaridan foydalanganingiz uchun tashakkur!"<<endl;
+			cout<<"----------------------------------------------"<<endl;
+			break;
+		}
+		else {
+			cout<<"Bunday bo'lim mavjud emas"<<endl;
+			cout<<"----------------------------------------------"<<endl;
+		}
+	}
+	
 
-
-	// while (true)1
-	// {
-	// 	bool sorov;
-	// 	cout<<"Navbatga yozilashni xohlaysizmi? (1/0): ";
-	// 	cin>>sorov;
-	// 	if(sorov){
-	// 		// string name; cout<<"Ism: "; getline(cin, name);
-	// 		// int id; cout<<"Bolim (index): "; cin>>id;
-	// 		tbc.addClient("Mustafo", 1);
-	// 	}
-	// 	else{
-	// 		break;
-	// 	}
-	// }
 	
 	
 	return 0;
@@ -123,4 +176,16 @@ void Bank::getInfo(){
 			cout<<setw(3)<<id+1<<". "<<b.second<<": "<<navbat.at(id).size()<<endl;
 			id++;
 		}
+		cout<<endl;
+}
+void Bank::getInfo(int b){
+	string bankName = bank.getAsMap().at(b);
+	if(!navbat.at(b-1).empty()){
+		cout<<bankName<<" => "<<navbat.at(b-1).front().getName()<<" ga hizmat ko'rsatilmoqda..."<<endl;
+	}
+	else {
+		cout<<bankName<<" => "<<"Navbat bosh"<<endl;
+	}
+	cout<<"----------------------------------------------"<<endl;
+	
 }
